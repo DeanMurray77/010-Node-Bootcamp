20c1e3b4455e73c72c92c90de3c43a57ae736a3c
Dean <dean.l.murray@hotmail.com>
2019-04-26 07:06:52 -0600

    Test File I/O

---
+++ 001-Notes-App/app.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'My name is Dean');
+// fs.appendFileSync('notes.txt', '\nThis file was created by NodeJS!');
+
+fs.appendFileSync('notes.txt', '\nHere is another line added to the file...');
