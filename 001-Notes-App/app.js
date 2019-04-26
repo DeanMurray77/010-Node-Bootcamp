a7cf30f8687010f26331f1bc4e572569741eae93
Dean <dean.l.murray@hotmail.com>
2019-04-26 16:46:42 -0600

    Test Validator

--- 001-Notes-App/app.js
+++ 001-Notes-App/app.js
@@ -2,3 +2,6 @@ const getNotes = require('./notes.js');
 const validator = require('validator');
 
 console.log(getNotes());
+
+console.log(validator.isEmail('dean@somesitecom'));
+console.log(validator.isEmail('dean@somesite.com'));
