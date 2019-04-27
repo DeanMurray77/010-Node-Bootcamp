e337890fc5cdca495e73d11e4429ac9371ed8afd
Dean <dean.l.murray@hotmail.com>
2019-04-27 16:16:07 -0600

    Add in additional yargs commands
    add and remove

--- 001-Notes-App/app.js
+++ 001-Notes-App/app.js
@@ -3,7 +3,22 @@ const yargs = require('yargs');
 
 const getNotes = require('./notes.js');
 
-console.log(process.argv);
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note!')
+    }
+});
+
 console.log(yargs.argv);
 
 //1,3,6,8 points where the git color key changes...
