359db28a90cc23d9dcff87b0983b80722eaafe6c
Dean <dean.l.murray@hotmail.com>
2019-04-26 21:37:17 -0600

    Added in Chalk example usages

--- 001-Notes-App/app.js
+++ 001-Notes-App/app.js
@@ -3,7 +3,9 @@ const validator = require('validator');
 const chalk = require('chalk');
 
 console.log(getNotes());
+console.log(chalk.greenBright('Success'));
+console.log(chalk.red.bold.inverse("Error"));
+console.log("what?");
 
 
-
-//4:52
+//1,3,6,8 points where the git color key changes...
