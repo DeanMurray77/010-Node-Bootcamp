f91f94f8b3160aa29fa1c25894dc3e6fc2d589bb
Dean <dean.l.murray@hotmail.com>
2019-04-29 12:37:22 -0600

    New example Read, Change, Write

--- Archived-Items/1-json.js
+++ Archived-Items/1-json.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 
+const dataBuffer = fs.readFileSync('data.json');
+let dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+data.name = 'Dean';
+data.age = '42';
+
+dataJSON = JSON.stringify(data);
+fs.writeFileSync('newData.json', dataJSON);
+
+
+
 // const book = {
 //     title: "Torn",
 //     author: "Dean Murray"
@@ -9,9 +21,9 @@ const fs = require('fs');
 
 // fs.writeFileSync('1-json.json', bookJSON);
 
-const dataBuffer = fs.readFileSync('1-json.json');
-const dataJSON = dataBuffer.toString();
-const parsedData = JSON.parse(dataJSON);
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const parsedData = JSON.parse(dataJSON);
 
-console.log(parsedData.author);
-console.log(parsedData.title);
+// console.log(parsedData.author);
+// console.log(parsedData.title);
