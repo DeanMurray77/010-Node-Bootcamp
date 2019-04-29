0745ad1209f9b754a040c62d7d937e91260ca60a
Dean <dean.l.murray@hotmail.com>
2019-04-29 12:20:52 -0600

    Read from file
    Commented out the write code and read from the file that was previously written.

--- Archived-Items/1-json.js
+++ Archived-Items/1-json.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
-const book = {
-    title: "Torn",
-    author: "Dean Murray"
-}
+// const book = {
+//     title: "Torn",
+//     author: "Dean Murray"
+// }
 
-const bookJSON = JSON.stringify(book);
+// const bookJSON = JSON.stringify(book);
 
-fs.writeFileSync('1-json.json', bookJSON);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+const jsonFile = fs.readFileSync('1-json.json');
+
+const parsedData = JSON.parse(jsonFile);
+console.log(parsedData.author);
+console.log(parsedData.title);
