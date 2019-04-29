72821a1d9adb3449e74fa04c6a6be5674b3075d0
Dean <dean.l.murray@hotmail.com>
2019-04-29 16:45:35 -0600

    Extend Notes App
    Added loadNotes, saveNotes, addNote functions.
    
    Extended getNotes.
    
    Added error checking to loadNotes and addNotes.
    
    Doesn't break if there isn't a notes.json to load from.
    
    Doesn't allow for duplicate titles to be used on new notes.

--- 001-Notes-App/app.js
+++ 001-Notes-App/app.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
 
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Create Add command
 yargs.command({
@@ -20,8 +20,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ', argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
--- 001-Notes-App/notes.js
+++ 001-Notes-App/notes.js
@@ -1,5 +1,50 @@
+const fs = require('fs');
+
 const getNotes = () => {
-    return "Your notes:"
+    const notes = loadNotes();
+    return notes;
 }
 
-module.exports = getNotes;
+const saveNotes = (notes) => {
+    const notesJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', notesJSON);
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        const notes = JSON.parse(dataJSON);
+        return notes;
+    } catch(err) {
+        return []; //if no file exists, return an empty array
+    }
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+
+    const filteredNotes = notes.filter((note) => {
+        return note.title === title;
+    })
+
+    if(!filteredNotes.length) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+    } else {
+        console.log("A note already exists with that title");
+    }
+
+}
+
+const removeNote = () => {
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
