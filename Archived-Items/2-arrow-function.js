8ef9fbbe139487270068194da2003573a4003842
Dean <dean.l.murray@hotmail.com>
2019-04-29 20:24:53 -0600

    Experimenting more with Arrow Functions

--- Archived-Items/2-arrow-function.js
+++ Archived-Items/2-arrow-function.js
@@ -1,3 +1,26 @@
 const square = (x) => x * x;
 
-console.log(square(4));
+console.log(square(6));
+
+const event = {
+    name: "Birthday Party",
+    printGuestList: function () {
+        console.log("Guest list for " + this.name);
+    }
+}
+
+
+event.printGuestList();
+
+const es6Event = {
+    name: "Wedding",
+    guestList: ['Dean', 'Dizzy', 'Elric'],
+    printGuestList() {
+        console.log("Guest list for " + this.name);
+        this.guestList.forEach((guest)=> {
+        console.log(guest + " Is attending the " + this.name);
+        })
+    }
+}
+
+es6Event.printGuestList();
