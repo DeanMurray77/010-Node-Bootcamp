d56a92f3e9d1a2232363b37b88f601c7b283f451
Dean <dean.l.murray@hotmail.com>
2019-05-02 06:47:38 -0600

    Get current conditions, log temp and % change of rain.

--- 002-Weather-App/app.js
+++ 002-Weather-App/app.js
@@ -3,7 +3,7 @@ const config = require('./config');
 
 let location = "37.8267,-122.4233";
 
-request( {url: config.url+location}, (error, response) => {
-    const parsedResponse = JSON.parse(response.body);
-    console.log(parsedResponse.currently);    
-} )
+request( {url: config.url+location, json: true}, (error, response) => {
+    console.log(response.body.currently);
+    console.log(`Currently it is ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% change of rain.`);
+})
