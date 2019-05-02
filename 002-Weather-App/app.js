4838616c0614a4f762ef2e31a7cc171a39222285
Dean <dean.l.murray@hotmail.com>
2019-05-02 06:34:19 -0600

    Parse and log the response
    (Current weather conditions)

--- 002-Weather-App/app.js
+++ 002-Weather-App/app.js
@@ -4,6 +4,6 @@ const config = require('./config');
 let location = "37.8267,-122.4233";
 
 request( {url: config.url+location}, (error, response) => {
-//    console.log(error);
-   console.log(response);
+    const parsedResponse = JSON.parse(response.body);
+    console.log(parsedResponse.currently);    
 } )
