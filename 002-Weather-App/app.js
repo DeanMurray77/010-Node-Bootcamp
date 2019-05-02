94510f4c5670b1e3d7a17f88483c7dba64bda146
Dean <dean.l.murray@hotmail.com>
2019-05-02 17:33:56 -0600

    Added coordinate lookup request

--- 002-Weather-App/app.js
+++ 002-Weather-App/app.js
@@ -2,9 +2,23 @@ const request = require('request');
 const config = require('./config');
 
 let location = "41.0735,-111.9532";
-
+// console.log(config.url+location)
 request( {url: config.url+location, json: true}, (error, response) => {
     console.log(response.body.daily.data[0].summary);
     console.log(`Currently it is ${response.body.currently.temperature} degrees out.`);
-    console.log(`There is a ${response.body.currently.precipProbability}% change of rain.`);
+    console.log(`There is a ${response.body.currently.precipProbability}% chance of rain.`);
 })
+
+let lookupLocation = 'Layton Utah';
+let encodedLocation = encodeURI(lookupLocation);
+let mapBoxFinalURL = config.mapBoxURL+encodedLocation+'.json'+config.mapBoxAPIKey;
+request( {url: mapBoxFinalURL, json: true}, (error, response) => {
+    if(error) {
+        console.log("Error, no such place found");
+    } else {
+        console.log(response.body.features[0].place_name);
+        console.log(response.body.features[0].center);
+    }
+} )
+
+// console.log(mapBoxFinalURL);
