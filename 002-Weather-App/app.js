dd95d870ab56a5d80543d161b7916e3e385f1522
Dean <dean.l.murray@hotmail.com>
2019-05-02 06:57:36 -0600

    Change formatting of the response. Add option to change units.

--- 002-Weather-App/app.js
+++ 002-Weather-App/app.js
@@ -1,9 +1,10 @@
 const request = require('request');
 const config = require('./config');
 
-let location = "37.8267,-122.4233";
+let location = "41.0735,-111.9532";
+let APIOptions = '?units=si';
 
-request( {url: config.url+location, json: true}, (error, response) => {
-    console.log(response.body.currently);
-    console.log(`Currently it is ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% change of rain.`);
+request( {url: config.url+location+APIOptions, json: true}, (error, response) => {
+    console.log(`Currently it is ${response.body.currently.temperature} degrees out.`);
+    console.log(`There is a ${response.body.currently.precipProbability}% change of rain.`);
 })
