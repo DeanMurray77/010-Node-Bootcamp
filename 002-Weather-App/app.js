4ad726313e9b688046ed3dd70c4bb98fda4889c6
Dean <dean.l.murray@hotmail.com>
2019-05-15 09:04:30 -0600

    Clean up Callback Chaining. Accept User Input

--- 002-Weather-App/app.js
+++ 002-Weather-App/app.js
@@ -1,18 +1,19 @@
-
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Saint George, UT', (error, data) => { //(error, data) is just the response back via callback
-    if(error) {
-        console.log("Error:", error);
-    } else {
-        (forecast(data.latitude, data.longitude, (error, data) => {
-            if(error) {
-                console.log('Error', error)
-            } else {
-                console.log('Data', data)
-            }
-        }))
-    }
+const location = process.argv[2];
 
+geocode(location, (error, data) => { //(error, data) is just the response back via callback
+    if(error) {
+        return console.log("Error:", error);
+    }
+    
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error) {
+            return console.log('Error', error)
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+    })
 })
