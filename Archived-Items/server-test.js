76710397819bc45c6639c965b4143c920df64a43
Dean <dean.l.murray@hotmail.com>
2019-05-23 14:20:47 -0600

    Changed

--- Archived-Items/server-test.js
+++ Archived-Items/server-test.js
@@ -1,45 +1,61 @@
-// Original
+// Changed
 (function() {
 	// populate the 'data' object
 	// e.g., data.table = $sp.getValue('table');
+	// modified by Earl Lewis Feb. 17, 2017
+	// commented out cost and resource fields and added budget fields for report display
 	
 	function getIcon(value) {
 		if ( value == 'green' )
-			return 'icon-success-circle';
+			return 'fa-check-circle';
 		else if ( value == 'yellow' )
-			return 'icon-not-started-circle';
+			return 'fa-minus-circle';
+		else if ( value == 'na' )
+			return 'fa-ban';
 		else
-			return 'icon-warning-circle';
+			return 'fa-exclamation-circle';
 	}
 	
 	var s = new PmProjectStatusReport();
 	data.limit = 11;
-	var endDate = $sp.getParameter('sysparm_status_date') || (new GlideDate()).getDisplayValue();
+	var endDate = new GlideDate();
 	
 	var statuses = s.getStatuses($sp.getParameter('sysparm_sys_id'), endDate, data.limit);
 
 	data.categories = [];
 	data.categories.push({label: gs.getMessage('Overall'), name: 'overall'});
 	data.categories.push({label: gs.getMessage('Schedule'), name: 'schedule'});
-	data.categories.push({label: gs.getMessage('Cost'), name: 'cost'});
-	data.categories.push({label: gs.getMessage('Resources'), name: 'resources'});
+	//data.categories.push({label: gs.getMessage('Cost'), name: 'cost'});
+	//data.categories.push({label: gs.getMessage('Resources'), name: 'resources'});
 	data.categories.push({label: gs.getMessage('Scope'), name: 'scope'});
+	data.categories.push({label: gs.getMessage('Budget'), name: 'budget'});
 		
 	data.statuses = {};
 	data.statuses.overall = [];
-	data.statuses.cost = [];
+	//data.statuses.cost = [];
 	data.statuses.schedule = [];
 	data.statuses.scope = [];
-	data.statuses.resources = [];
+	//data.statuses.resources = [];
+	data.statuses.budget = [];
 	data.dates = [];
 	
 	for ( var i=0; i < statuses.length; i++ ) {
 		data.statuses.overall.push({icon: getIcon(statuses[i].overall), comment: statuses[i].comments});
-		data.statuses.cost.push({icon: getIcon(statuses[i].cost), comment: statuses[i].cost_comments});		
-		data.statuses.resources.push({icon: getIcon(statuses[i].resources), comment: statuses[i].resources_comments});
+		//data.statuses.cost.push({icon: getIcon(statuses[i].cost), comment: statuses[i].cost_comments});		
+		//data.statuses.resources.push({icon: getIcon(statuses[i].resources), comment: statuses[i].resources_comments});
 		data.statuses.schedule.push({icon: getIcon(statuses[i].schedule), comment: statuses[i].schedule_comments});
 		data.statuses.scope.push({icon: getIcon(statuses[i].scope), comment: statuses[i].scope_comments});
+		data.statuses.budget.push({icon: getIcon(statuses[i].budget), comment: statuses[i].budget_comments});
 		data.dates.push(statuses[i].date);
 	}
 	//gs.info("cost_comment: " + (new JSON()).encode(data.statuses.cost));
+	
+	data.datesArray = [];
+	for(i=0; i<data.dates.length;i++) {
+		data.datesArray.push(i);
+	}
+	
+	//Debugging Line - remove later
+	console.log(data);
+	
 })();
