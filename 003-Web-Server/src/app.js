78a68b8d256c72b3ec6704904c42d4551a814ac3
Dean <dean.l.murray@hotmail.com>
2019-06-03 17:02:02 -0600

    Added about & weather routes

--- 003-Web-Server/src/app.js
+++ 003-Web-Server/src/app.js
@@ -10,6 +10,14 @@ app.get('/help', (req, res) => {
     res.send("Here's the help");
 })
 
+app.get('/about', (req, res) => {
+    res.send("Here is the about page");
+})
+
+app.get('/weather', (req, res) => {
+    res.send("Here is the weather, so much weather");
+})
+
 app.listen(3000, ()=>{
     console.log("Server is up on port 3000");
 });
