3ee91c22a6f85b61a42966d33a421f5f5c4fab19
Dean <dean.l.murray@hotmail.com>
2019-06-03 17:13:24 -0600

    Return HTML rather than just text

--- 003-Web-Server/src/app.js
+++ 003-Web-Server/src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('', (req, res) => {
-    res.send("Hello express!");
+    res.send("<h1>Weather App</h1><p>All weather, all of the time</p>");
 })
 
 app.get('/help', (req, res) => {
