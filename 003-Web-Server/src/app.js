460ee4e78579226214bdb70aaef514fe76cdd82d
Dean <dean.l.murray@hotmail.com>
2019-06-06 17:28:23 -0600

    Make help page dynamic (injecting via the object)

--- 003-Web-Server/src/app.js
+++ 003-Web-Server/src/app.js
@@ -22,7 +22,10 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/help', (req, res) => {
-    res.render('help');
+    res.render('help', {
+        title: 'Help',
+        helpMessage: 'If you need help, please email support@site.com'
+    });
 })
 
 app.get('/weather', (req, res) => {
