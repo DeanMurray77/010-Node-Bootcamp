26271da7c7b23225d01beeddd8be2b0ddb1ecb4a
Dean <dean.l.murray@hotmail.com>
2019-06-20 17:05:10 -0600

    Fetching external API from the client-side javascript

--- 003-Web-Server/public/js/app.js
+++ 003-Web-Server/public/js/app.js
@@ -1,2 +1,8 @@
 //Message to prove that the client-side javascript is being loaded
 console.log("Client side javascript file is loaded");
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    });
+})
