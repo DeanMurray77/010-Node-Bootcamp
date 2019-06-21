6cbcd1f9c8f5df8c3db2775df36a78851f7bc231
Dean <dean.l.murray@hotmail.com>
2019-06-21 06:48:09 -0600

    Clean up GitIgnore. Clean up app.js
    Removed unnecessary logging

--- 003-Web-Server/public/js/app.js
+++ 003-Web-Server/public/js/app.js
@@ -1,6 +1,3 @@
-//Message to prove that the client-side javascript is being loaded
-console.log("Client side javascript file is loaded");
-
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data);
