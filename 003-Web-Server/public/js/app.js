6efae43d39e98a37d6663c6d66bc48b7c4d4d6ef
Dean <dean.l.murray@hotmail.com>
2019-06-26 16:27:03 -0600

    Wire up input to API route
    Gave form a name.
    Moved the fetch statement inside the event listener and made it accept the form input and pass as part of the API call to the /weather route.

--- 003-Web-Server/public/js/app.js
+++ 003-Web-Server/public/js/app.js
@@ -1,16 +1,17 @@
-fetch('/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if(!data.error) {
-            console.log(`The forecast for ${data.location} is ${data.forecast}`);
-        } else {
-            console.log(data.error);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form');
 
-weatherForm.addEventListener('submit', () => {
-    // event.preventDefault();
-    console.log('The button was clicked');
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    let route = '/weather?address=';
+    route += event.target.elements.inputLocation.value;
+    fetch(route).then((response) => {
+        response.json().then((data) => {
+            if(!data.error) {
+                console.log(`The forecast for ${data.location} is ${data.forecast}`);
+            } else {
+                console.log(data.error);
+            }
+        })
+    })
 })
