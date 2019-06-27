5704ad734806dd7d787d058093b70e2783c06aea
Dean <dean.l.murray@hotmail.com>
2019-06-27 07:07:54 -0600

    Clean up appearance. Remove previous message while loading.

--- 003-Web-Server/public/js/app.js
+++ 003-Web-Server/public/js/app.js
@@ -7,12 +7,13 @@ const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
     messageOne.textContent ='Loading Results';
+    messageTwo.textContent = '';
     
     const location = search.value;
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(!data.error) {
-                messageOne.textContent = `The forecast for ${data.location} is`;
+                messageOne.textContent = `The forecast for ${data.location}:`;
                 messageTwo.textContent = `${data.forecast}`
             } else {
                 messageOne.textContent = data.error;
