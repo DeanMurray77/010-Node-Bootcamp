6ae2d4f513dd7cd59df63c3e9162aac23e2ea3fc
Dean <dean.l.murray@hotmail.com>
2019-07-10 12:14:02 -0600

    Promise experiments.

---
+++ Archived-Items/8.1-promise-chaining.js
@@ -0,0 +1,27 @@
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1,2).then((sum) => {
+    console.log(sum);
+    add(sum, 5).then((sum2) => {
+        console.log(sum2);
+    }).catch((e2) => {
+        console.log(e2);
+    })
+}).catch((e) => {
+    console.log(e);
+})
+
+add(3,7).then((sum) => {
+    console.log(sum);
+    return add(sum, 10);
+}).then((sum2) => {
+    console.log(sum2);
+}).catch((e) => {
+    console.log(e);
+})
